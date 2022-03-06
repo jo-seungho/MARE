1812581db141b3d79e8ba4c513a5361dfa8560b5
K_ROCK_ <75164778+CHOI-K-ROCK@users.noreply.github.com>
2022-03-06 15:25:16 +0900

    Revert "Feature/add ingredients"

--- server/controllers/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (userInfo) => {
-    return jwt.sign({
-        id: userInfo._id
-    }, process.env.JWT_SECRET_KEY, { expiresIn: '24h' });
-}
-
-module.exports = generateToken;
